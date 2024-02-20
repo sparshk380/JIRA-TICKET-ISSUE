6b0fb37b9d9fba669675e8d2d34cd8f26d80ceaa
Sparsh Khandelwal <82154970+sparshk380@users.noreply.github.com>
2024-02-20 16:30:34 +0530

    Update commit-msg.sh

--- .github/scripts/commit-msg.sh
+++ .github/scripts/commit-msg.sh
@@ -9,5 +9,4 @@ commit_message=$(cat $1)
 # Check if the commit message contains a Jira ticket reference
 if ! [[ $commit_message =~ $JIRA_PATTERN ]]; then
     echo "Error: Please add a Jira ticket reference in the commit message."
-    exit 1
 fi
