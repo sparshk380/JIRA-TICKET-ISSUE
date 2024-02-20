52cde04ba6b8ad317079ffee1d34bcbfff6832a9
Sparsh Khandelwal <82154970+sparshk380@users.noreply.github.com>
2024-02-20 16:13:22 +0530

    HECO-1343 Update commit-msg.sh

--- .github/scripts/commit-msg.sh
+++ .github/scripts/commit-msg.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-commit_msg_file="$1"
-commit_msg=$(cat "$commit_msg_file")
+# Regular expression to match Jira ticket references
+JIRA_PATTERN="[A-Z]+-[0-9]+"
 
-# Regular expression to match Jira ticket pattern (e.g., ABC-123)
-jira_pattern="[A-Z]+-[0-9]+"
+# Read the commit message from stdin
+commit_message=$(cat $1)
 
-if [[ ! $commit_msg =~ $jira_pattern ]]; then
-  echo "Error: Please add a Jira ticket reference in the commit message." >&2
-  exit 1
+# Check if the commit message contains a Jira ticket reference
+if ! [[ $commit_message =~ $JIRA_PATTERN ]]; then
+    echo "Error: Please add a Jira ticket reference in the commit message."
+    exit 1
 fi
+
+# If a valid Jira ticket reference is found, allow the commit to proceed
+exit 0
